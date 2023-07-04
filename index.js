59fc203c4aa16e3cdece5447d351c00c2a69424e
travelite <rendrabootcamp@gmail.com>
2023-07-05 01:26:18 +0700

    upload file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import { logRequestPrimary } from "./src/middleware/logs.js";
 import usersRouter from "./src/routes/users.js";
+import { upload } from "./src/middleware/multer.js";
+
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
@@ -11,6 +13,11 @@ app.use(express.json());
 app.use("/img", express.static("public/img"));
 
 app.use("/users", usersRouter);
+app.post("/upload", upload.single("photo"), (req, res) => {
+	res.json({
+		message: "Upload berhasil",
+	});
+});
 
 app.listen(PORT, () => {
 	console.log(`Server running ${PORT}`);
---
+++ src/middleware/multer.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "public/img");
+	},
+	filename: (req, file, cb) => {
+		const timestapm = new Date().getTime();
+		const filename = file.originalname;
+		// const extension = path.extname(file.originalname);
+
+		cb(null, `${timestapm}-${filename}`);
+	},
+});
+
+const upload = multer({ storage: storage });
+
+export { upload };
